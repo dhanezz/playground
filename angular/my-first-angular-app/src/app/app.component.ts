ca7716c96e539f2f9625ebb31a16990b9c635ddb
dhanezz <erikacanupin@gmail.com>
2025-02-24 22:23:04 +0100

    refactor: :construction: first experiencies in adding new components
    Explored the changes between the module system of angular and the standalone system used in Angular 19

--- angular/my-first-angular-app/src/app/app.component.ts
+++ angular/my-first-angular-app/src/app/app.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+// COMPONENTS IMPORTS
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { LoginComponent } from './user-auth/login/login.component';
+
+// NOTE: Common Module is imported to use the *ngIf directive
+
+// NOTE: @Component class decorator is used to define the metadata for the component class. 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MyNewComponentComponent, LoginComponent], 
+  standalone: true, // NOTE: standalone is set to true to so it doesn't require a parent component and can import other components directly
   templateUrl: './app.component.html',
+  // template: `
+  //   <h2>My First Angular App</h2>
+  //   <p *ngIf="initialized">This text is visible if the condition is true.</p> 
+  //   <p *ngIf="destroys">Component Destroyed</p>
+  // `,
   styleUrl: './app.component.less'
 })
-export class AppComponent {
-  title = 'my-first-angular-app';
+
+export class AppComponent implements OnInit {
+  title:string = 'my-first-angular-app';
+  initialized: boolean = false;
+  destroys: boolean = false;
+
+  //@Input() data:string;
+  //@Output() params:string;
+  
+  constructor() {
+    console.log('AppComponent constructor called');
+  }
+
+  ngOnInit(): void {
+    this.initialized = true;
+    console.log('AppComponent ngOnInit called');
+  }
+
+  ngOnDestroy(): void {
+    this.destroys = true;
+    console.log('AppComponent ngOnDestroy called');
+  }
 }
